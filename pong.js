929e61bd318875202b55d53781830d8bc4359ced
Anushka Levaku <AnushkaLevaku@Anushkas-MacBook-Pro-3.local>
2024-01-18 10:32:48 -0500

    Initial JS commit

---
+++ pong.js
@@ -0,0 +1,121 @@
+let board;
+let boardWidth = 500;
+let boardHeight = 500;
+let context;
+
+let playerWidth = 10;
+let playerHeight = 50;
+let playerVelocityY = 0;
+
+let ballWidth = 10;
+let ballHeight = 10;
+
+
+let player1 = {
+    x : 10,
+    y : boardHeight/2,
+    width : playerWidth,
+    height : playerHeight,
+    velocityY : playerVelocityY
+
+}
+
+let player2 = {
+    x : boardWidth - 10 - playerWidth,
+    y : boardHeight/2,
+    width : playerWidth,
+    height : playerHeight,
+    velocityY : playerVelocityY
+}
+
+let ball = {
+    x : boardWidth/2,
+    y : boardHeight/2,
+    width : ballWidth,
+    height : ballHeight,
+    velocityX : 1,
+    velocityY : 2
+}
+
+window.onload = function() {
+    board = document.getElementById("board");
+    board.height = boardHeight;
+    board.width = boardWidth;
+    context = board.getContext("2d");
+
+    context.fillStyle = "skyblue";
+    context.fillRect(player1.x, player1.y, player1.width, player1.height);
+
+    requestAnimationFrame(update)
+    document.addEventListener("keyup", movePlayer);
+}
+
+function update() {
+    requestAnimationFrame(update);
+    context.clearRect(0, 0, board.width, board.height);
+
+    context.fillStyle = "skyblue";
+    let nextPlayer1Y = player1.y + player1.velocityY;   
+    if(!outOfBounds(nextPlayer1Y)) {
+        player1.y = nextPlayer1Y;
+    }
+    context.fillRect(player1.x, player1.y, player1.width, player1.height);
+
+    let nextPlayer2Y = player2.y + player2.velocityY;   
+    if(!outOfBounds(nextPlayer2Y)) {
+        player2.y = nextPlayer2Y;
+    }    context.fillRect(player2.x, player2.y, player2.width, player2.height);
+
+    context.fillStyle = "white";
+    ball.x += ball.velocityX;
+    ball.y += ball.velocityY;
+    context.fillRect(ball.x, ball.y, ball.width, ball.height);
+
+    //ball.y is top left corner silly --> to reverse direction if ball hits bounds
+    if (ball.y <= 0 || ball.y + ball.height >= boardHeight) {
+        ball.velocityY *= -1;
+    }
+
+    if(detectCollision(ball, player1)) {
+        if (ball.x < player1.x + player1.width) {
+            ball.velocityX *= -1;
+        }
+    }
+    else if (detectCollision(ball, player2)) {
+        if (ball.x + ballWidth >= player2.x) {
+            ball.velocityX *= -1;
+        }
+    }
+}
+
+function outOfBounds(yPostion) {
+    if ((yPostion + playerHeight) > boardHeight || yPostion < 0) {
+        return true;
+    }
+    return false;
+
+}
+
+function movePlayer(e) {
+    if (e.code = "KeyW") {
+        player1.velocityY = -3;
+    }
+    else if (e.code == "KeyS") {
+        player1.velocityY = 3;
+    }
+    
+
+    if (e.code = "ArrowUp") {
+        player2.velocityY = -3;
+    }
+    else if (e.code == "ArrowDown") {
+        player2.velocityY = 3;
+    }
+}
+
+function detectCollision(a, b) {
+    return a.x < b.x + b.width && //a top-left corner doesn't exceed b top-right corner 
+           a.x + a.width > b.x && //a top-right corner does exceed b top-top corner 
+           a.y < b.y + b.height && //a top-left corner doesn't exceed b bottom-left corner 
+           a.y + a.height > b.y; //a bottom-left corner does exceed b top-left corner 
+}
